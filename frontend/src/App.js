855b5cb1a85c79d7dd5f43d02a3b449facac58a7
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-05-23 20:20:17 +0530

    side navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import SideNavbar from './components/SideNavbar';
 
 function App() {
   return (
     <div className="App">
-      Major Project
+      <SideNavbar />
     </div>
   );
 }
---
+++ frontend/src/components/SideNavbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { styled, Box, Typography } from "@mui/material";
+import { Dashboard } from "@mui/icons-material/";
+
+const SideNav = styled(Box)({
+  height: "100%",
+  position: "fixed",
+  top: 0,
+  left: 0,
+  boxShadow: "0 0 15px rgba(0,0,0,0.75)",
+  clipPath: "inset(0px -15px 0px 0px)",
+  padding: "36px 0",
+  borderRadius: "0px 10px",
+  display: "flex",
+  flexDirection: "column",
+  color: '#27374D',
+  fontSize: '20px',
+  fontWeight: 500,
+});
+const Items = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: '20px',
+  padding: '10px 48px',
+});
+const Title = styled(Typography)({
+    fontSize: '24px',
+    fontWeight: '600',
+});
+
+function SideNavbar() {
+  return (
+    <SideNav>
+      <Items style={{backgroundColor: '#DDE6ED', color: '#526D82'}}>
+          <Dashboard fontSize="medium" />
+        <Title>
+          Dashboard
+        </Title>
+      </Items>
+
+      <Items></Items>
+    </SideNav>
+  );
+}
+
+export default SideNavbar;
